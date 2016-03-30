10f55c2652961d53118ca4b135b654b2e8db6c25
Joakim Wånggren <joakim.wanggren@schibsted.com>
2016-03-30 16:15:20 +0200

    Removed debug console.log

--- todos/list/handler.js
+++ todos/list/handler.js
@@ -7,7 +7,6 @@ var lib = require('../lib/todos');
 module.exports.handler = function(event, context) {
 
   lib.list(event, function(error, response) {
-    console.log(process.env);
     return context.done(error, response);
   });
 };
